bea9f3628e3b04468d7af7906d1718a879948386
Akshay Kumbhar <92714663+akakshay2607@users.noreply.github.com>
2023-04-02 10:53:35 +0530

    Update settings.py

--- office_emp_proj/settings.py
+++ office_emp_proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j8#y*jcmm+ihua=i$86gxrj)h*uc#g-%w&-jz!y&jg1k-f0no1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
